8aa27309c12a267c7461ef4690f771516176970f
Rachel Allen <rallenco13@gmail.com>
2022-07-09 09:38:42 -0600

    movie details rendering

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 import movieData from "./MovieData";
 import Movies from "./Movies";
+import MovieDetails from "./MovieDetails";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: movieData.movies,
+      isSelected: false,
     };
   }
+
+  selectMovie = (movie) => {
+    this.setState({ selectedMovie: movie.id, isSelected: true });
+  };
+
   render() {
     return (
       <>
         <nav>
           <h1>Rancid Tomatillos</h1>
         </nav>
-        <Movies movies={this.state.movies} />
+        {this.state.isSelected ? (
+          <MovieDetails movie={this.state.movies[0]} />
+        ) : (
+          <Movies movies={this.state.movies} />
+        )}
       </>
     );
   }
---
+++ src/MovieDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MovieDetails.css";
+
+const MovieDetails = ({ movie }) => {
+  return (
+    <div className="detail-card">
+      <h2> {movie.title}</h2>
+      <p>
+        {movie.release_date} {movie.average_rating} {movie.rating}
+      </p>
+      {/* {movie.genres.map((genre) => {
+        <div id={genre.id}>
+          <p>{genre}</p>
+        </div>;
+      })}
+      <h3>{movie.tagline}</h3>
+      <p>{movie.overview}</p>
+      <p>
+        {movie.budget} {movie.revenue}
+      </p> */}
+    </div>
+  );
+};
+
+export default MovieDetails
