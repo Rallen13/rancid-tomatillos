0ede88022b4554093f772f20a398d9c64810edda
Rachel Allen <rallenco13@gmail.com>
2022-07-09 11:41:52 -0600

    API calls wired up for all movie in App.js

--- src/App.js
+++ src/App.js
@@ -11,19 +11,30 @@ class App extends Component {
       loading: false,
       movies: [],
       selectedMovie: 0,
+      error: "",
     };
   }
+
+  checkForError = (response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error(response.status);
+    }
+  };
 
   componentDidMount() {
     this.setState({ loading: true });
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then((response) => this.checkForError(response))
       .then((response) => response.json())
       .then((data) => {
         this.setState({
           loading: false,
           movies: data.movies,
         });
-      });
+      })
+      .catch((err) => this.setState({ error: err.message }));
   }
 
   selectMovie = (movie) => {
@@ -40,9 +51,10 @@ class App extends Component {
         <nav>
           <h1>Rancid Tomatillos</h1>
         </nav>
+        {this.state.error && <h2>{this.state.error}</h2>}
         {this.state.selectedMovie ? (
           <MovieDetails
-            movie={this.state.movies[0]}
+            movie={this.state.selectedMovie}
             closeDetails={this.closeDetails}
           />
         ) : (
--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import "./MovieDetails.css";
 
-const MovieDetails = ({ movie, closeDetails }) => {
-  return (
-    <>
-      <button onClick={() => closeDetails()}>
-        <p>X</p>
-      </button>
-      <div className="detail-card">
-        <h2> {movie.title}</h2>
+class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      movie: {},
+    };
+  }
+
+  checkForError = (response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error(response.status);
+    }
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch(
+      `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movie}`
+    )
+      .then((response) => this.checkForError(response))
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          loading: false,
+          movie: data.movie,
+        });
+      })
+      .catch((err) => this.setState({ error: err.message }));
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <>
+        <button onClick={() => this.props.closeDetails()}>
+          <p>X</p>
+        </button>
+        <div className="detail-card">
+          <h2> {this.movie.title}</h2>
+          <p>
+            {this.movie.release_date} {this.movie.average_rating}{" "}
+            {this.movie.rating}
+          </p>
+          {/* {movie.genres.map((genre) => {
+          <div id={genre.id}>
+            <p>{genre}</p>
+          </div>;
+        })}
+        <h3>{movie.tagline}</h3>
+        <p>{movie.overview}</p>
         <p>
-          {movie.release_date} {movie.average_rating} {movie.rating}
-        </p>
-        {/* {movie.genres.map((genre) => {
-        <div id={genre.id}>
-          <p>{genre}</p>
-        </div>;
-      })}
-      <h3>{movie.tagline}</h3>
-      <p>{movie.overview}</p>
-      <p>
-        {movie.budget} {movie.revenue}
-      </p> */}
-      </div>
-    </>
-  );
-};
+          {movie.budget} {movie.revenue}
+        </p> */}
+        </div>
+      </>
+    );
+  }
+}
 
 export default MovieDetails;
