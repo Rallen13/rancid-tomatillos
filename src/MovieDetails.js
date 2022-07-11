11a169f6901daa86dbb32aa755f916d852980b25
Rachel Allen <rallenco13@gmail.com>
2022-07-10 22:32:31 -0600

    wrapped up movieDetails styling

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -11,31 +11,43 @@ const MovieDetails = ({ movie, closeDetails }) => {
   };
 
   return (
-    <div key={movie.id} style={backgroundImage} className="detail-container">
+    <div key={movie.id} style={backgroundImage}>
       <div className="detail-content">
         <div className="detail-close">
           <p className="detail-header">{movie.title}</p>
           <button onClick={() => closeDetails()} className="close-btn">
-            <p>X</p>
+            <span className="material-icons close">close</span>
           </button>
         </div>
         <div className="detail-card-container">
           <div className="detail-card">
-            <h2> {movie.title}</h2>
-            <p>
-              {movie.release_date} {movie.average_rating} {movie.rating}
+            <h2 className="detail-title"> {movie.title}</h2>
+            <p className="detail-date-rating">
+              {movie.release_date.slice(0, 4)}{" "}
+              <span className="seperator">|</span>{" "}
+              <span className="material-icons star">star</span>{" "}
+              {movie.average_rating.toFixed(1)}{" "}
+              <span className="seperator">|</span>{" "}
+              <span className="material-icons schedule">schedule</span>
+              {movie.runtime} min
             </p>
             <div className="genre-container">
               {movie.genres.map((genre) => {
                 return (
-                  <div key={genre}>
+                  <div key={genre} className="genre-tag">
                     <p>{genre}</p>
                   </div>
                 );
               })}
             </div>
             <h3>{movie.tagline}</h3>
-            <p>{movie.overview}</p>
+            <p className="detail-overview">{movie.overview}</p>
+            <div className="detail-money-container">
+              <p className="detail-money">
+                Budget: ${movie.budget} <span className="seperator">|</span>{" "}
+                Revenue: ${movie.revenue} 
+              </p>
+            </div>
           </div>
         </div>
       </div>
