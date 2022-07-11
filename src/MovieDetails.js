f6c2ced0d374472fe9217183f607b7ac2aec9c0e
Rachel Allen <rallenco13@gmail.com>
2022-07-10 20:55:51 -0600

    installed Mulish font and material-ui icons

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -4,10 +4,10 @@ import "./MovieDetails.css";
 const MovieDetails = ({ movie, closeDetails }) => {
   const backgroundImage = {
     background: `url(${movie.backdrop_path}) no-repeat center center fixed`,
-    "-webkit-background-size": "cover",
-    "-moz-background-size": "cover",
-    "-o-background-size": "cover",
-    "background-size": "cover",
+    WebkitBackgroundSize: "cover",
+    MozBackgroundSize: "cover",
+    OBackgroundSize: "cover",
+    backgroundSize: "cover",
   };
 
   return (
